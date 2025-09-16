52da7ef6cde00b1b8c201960fc060badeef18128
jayanth618 <jayanthpujari618@gmail.com>
2025-09-16 19:51:45 +0530

    Update user.java

--- user.java
+++ user.java
@@ -7,8 +7,9 @@ public class user {
 	user(String name,long accno){
 		this.name=name;
 		this.accno=accno;
-		System.out.println(this.name);
-		System.out.println(this.accno);
+		System.out.println("your name"+this.name);
+		System.out.println("your account number"+this.accno);
 	}
 
 }
+
